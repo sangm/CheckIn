1a90b07424359dbc89ba907cea147db26dbd7064
Sang Mercado <sang.mercado@gmail.com>
2016-01-15 10:45:23 +0900

    Removing AngularJS

--- angular-frontend/scripts/controllers/student.js
+++
@@ -1,9 +0,0 @@
-angular
-    .module('tutoringLoginApp')
-    .controller('StudentCtrl', function($scope, StudentFactory, StudentListService) {
-        $scope.students = StudentListService.getStudents(); 
-    
-        $scope.removeStudent = function(index) {
-            StudentListService.removeStudent(index);
-        };
-    });
